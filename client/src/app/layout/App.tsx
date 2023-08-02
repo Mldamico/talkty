004e9fe85e0eb675a2948a7794bfa40e2c5ff192
mldamico <matias.lioneldamico@gmail.com>
2023-08-02 09:43:34 -0300

    styles in nav and app with max width

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -14,11 +14,13 @@ function App() {
   return (
     <>
       <Nav />
-      <ul>
-        {activities?.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
-        ))}
-      </ul>
+      <div className="max-w-2xl mx-auto mt-24 lg:max-w-4xl xl:max-w-6xl">
+        <ul>
+          {activities?.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
--- client/src/app/layout/Nav.tsx
+++ client/src/app/layout/Nav.tsx
@@ -1,8 +1,9 @@
 export const Nav = () => {
   return (
-    <div className="fixed top-0 w-full text-white bg-black">
-      <div className="container flex items-center max-w-6xl gap-6 p-3 m-auto">
-        <header>
+    <div className="fixed top-0 w-full text-white nav">
+      <div className="flex items-center max-w-2xl gap-6 p-3 m-auto  lg:max-w-4xl xl:max-w-6xl">
+        <header className="flex items-center gap-3">
+          <img className="w-8" src="/assets/logo.png" alt="Logo" />
           <h3>Talkty</h3>
         </header>
         <div className="">Activities</div>
