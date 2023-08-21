5e841e6b1be5b2c3c49f2865fba1fa8c44f41bc8
mldamico <matias.lioneldamico@gmail.com>
2023-08-21 12:41:44 -0300

    Navbar with user

--- client/src/app/layout/Nav.tsx
+++ client/src/app/layout/Nav.tsx
@@ -1,27 +1,46 @@
 import { NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
-export const Nav = () => {
+export const Nav = observer(() => {
+  const {
+    userStore: { user, logout },
+  } = useStore();
   return (
     <div className="fixed top-0 w-full text-white nav">
-      <div className="flex items-center max-w-2xl gap-6 p-3 m-auto lg:max-w-4xl xl:max-w-6xl">
-        <header>
-          <NavLink to="/" className="flex items-center gap-3">
-            <img className="w-8" src="/assets/logo.png" alt="Logo" />
-            <h3>Talkty</h3>
+      <div className="flex items-center justify-between max-w-2xl p-3 m-auto lg:max-w-4xl xl:max-w-6xl">
+        <div className="flex flex-row items-center gap-6">
+          <header>
+            <NavLink to="/" className="flex items-center gap-3">
+              <img className="w-8" src="/assets/logo.png" alt="Logo" />
+              <h3>Talkty</h3>
+            </NavLink>
+          </header>
+          <div className="">
+            <NavLink to="/activities">Activities</NavLink>
+          </div>
+          <div className="">
+            <NavLink to="/errors">Errors</NavLink>
+          </div>
+          <NavLink to="/createactivity">
+            <button className="p-2 bg-green-600 rounded-md">
+              Create Activity
+            </button>
           </NavLink>
-        </header>
-        <div className="">
-          <NavLink to="/activities">Activities</NavLink>
         </div>
-        <div className="">
-          <NavLink to="/errors">Errors</NavLink>
+
+        <div className="flex flex-row items-center gap-3">
+          <div className="flex gap-2">
+            <NavLink to={`/profile/${user?.username}`}>My Profile</NavLink>
+            <button onClick={logout}>Logout</button>
+          </div>
+          <img
+            src={user?.image || "/assets/user.png"}
+            alt={user?.displayName}
+            className="w-8 h-8 rounded-full"
+          />
         </div>
-        <NavLink to="/createactivity">
-          <button className="p-2 bg-green-600 rounded-md">
-            Create Activity
-          </button>
-        </NavLink>
       </div>
     </div>
   );
-};
+});
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export const HomePage = () => {
+export const HomePage = observer(() => {
+  const { userStore } = useStore();
   return (
     <div className="flex flex-col items-center justify-center masthead">
       <div>
@@ -8,14 +11,25 @@ export const HomePage = () => {
           <img className="w-14" src="/assets/logo.png" alt="logo" />
           Talkty
         </h1>
-        <h2 className="text-white">Welcome to Talkty</h2>
-        <Link
-          className="flex justify-center py-2 mt-2 text-white border-2 border-white"
-          to="/login"
-        >
-          Login
-        </Link>
+        {userStore?.isLoggedIn ? (
+          <>
+            <h2 className="text-white">Welcome to Talkty</h2>
+            <Link
+              className="flex justify-center py-2 mt-2 text-white border-2 border-white"
+              to="/activities"
+            >
+              Go to Activities!
+            </Link>
+          </>
+        ) : (
+          <Link
+            className="flex justify-center py-2 mt-2 text-white border-2 border-white"
+            to="/login"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
-};
+});
