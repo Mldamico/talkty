771cf133423ead46d574a5a176b8f624d0f6b690
mldamico <matias.lioneldamico@gmail.com>
2023-08-28 09:19:24 -0300

    Upload and delete methods for photo

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -35,6 +35,7 @@ public static class ApplicationServiceExtensions
         services.AddValidatorsFromAssemblyContaining<Create>();
         services.AddHttpContextAccessor();
         services.AddScoped<IUserAccessor, UserAccessor>();
+        services.AddScoped<IPhotoAccessor, PhotoAccessor>();
         services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
         
         return services;
---
+++ Application/Photos/PhotoUploadResult.cs
@@ -0,0 +1,7 @@
+namespace API.Photos;
+
+public class PhotoUploadResult
+{
+    public string PublicId { get; set; }
+    public string Url { get; set; }
+}
