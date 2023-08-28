7946fae23521791f18e3d3dd3cbf9ff9ea586628
mldamico <matias.lioneldamico@gmail.com>
2023-08-28 09:05:19 -0300

    Cloudinary installation

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -4,6 +4,7 @@ using Application.Core;
 using Application.Interfaces;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,6 +35,8 @@ public static class ApplicationServiceExtensions
         services.AddValidatorsFromAssemblyContaining<Create>();
         services.AddHttpContextAccessor();
         services.AddScoped<IUserAccessor, UserAccessor>();
+        services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+        
         return services;
     }
 }
