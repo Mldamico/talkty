13df9fef180490b79ec619678d571ff881474393
mldamico <matias.lioneldamico@gmail.com>
2023-08-31 09:52:35 -0300

    Showing data for profile in header

--- client/src/features/profiles/ProfileHeader.tsx
+++ client/src/features/profiles/ProfileHeader.tsx
@@ -1,13 +1,20 @@
-export const ProfileHeader = () => {
+import { observer } from "mobx-react-lite";
+import { Profile } from "../../types/profile";
+
+interface Props {
+  profile: Profile;
+}
+
+export const ProfileHeader = observer(({ profile }: Props) => {
   return (
     <div className="flex justify-between p-3 bg-white shadow-md">
       <div className="flex items-center gap-4">
         <img
-          src={"/assets/user.png"}
-          alt="User"
+          src={profile.image || "/assets/user.png"}
+          alt={profile.displayName || "user"}
           className="w-24 rounded-full"
         />
-        <h1 className="text-4xl">Display name</h1>
+        <h1 className="text-4xl">{profile.displayName}</h1>
       </div>
       <div className="flex flex-col">
         <div className="flex gap-10 border-b border-gray-600">
@@ -24,4 +31,4 @@ export const ProfileHeader = () => {
       </div>
     </div>
   );
-};
+});
--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,11 +1,27 @@
+import { observer } from "mobx-react-lite";
 import { ProfileContent } from "./ProfileContent";
 import { ProfileHeader } from "./ProfileHeader";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useStore } from "../../app/stores/store";
+import { Loading } from "../../app/layout/Loading";
 
-export const ProfilePage = () => {
+export const ProfilePage = observer(() => {
+  const { username } = useParams<{ username: string }>();
+  const { profileStore } = useStore();
+  const { loadingProfile, loadProfile, profile } = profileStore;
+
+  useEffect(() => {
+    if (username) {
+      loadProfile(username);
+    }
+  }, [loadProfile, username]);
+
+  if (loadingProfile) return <Loading content="Loading..." />;
   return (
     <div>
-      <ProfileHeader />
+      {profile && <ProfileHeader profile={profile} />}
       <ProfileContent />
     </div>
   );
-};
+});
