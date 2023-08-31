af9679a3b780b8a90978f538d88fe071131bbe20
mldamico <matias.lioneldamico@gmail.com>
2023-08-31 16:37:24 -0300

    Get current user from profile and button to toggle add photo

--- client/src/features/profiles/ProfilePhoto.tsx
+++ client/src/features/profiles/ProfilePhoto.tsx
@@ -1,19 +1,42 @@
 import { observer } from "mobx-react-lite";
 import { Profile } from "../../types/profile";
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
 
 interface Props {
   profile: Profile;
 }
 export const ProfilePhoto = observer(({ profile }: Props) => {
+  const {
+    profileStore: { isCurrentUser },
+  } = useStore();
+
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
   return (
     <div>
-      <h1>Photos</h1>
-      <div className="grid gap-3 grid-cols-5">
-        {profile.photos?.map((photo) => (
-          <div key={photo?.id}>
-            <img src={photo?.url} alt="Photo" />
+      <div className="flex justify-between items-center">
+        <h1>Photos</h1>
+        {isCurrentUser && (
+          <button
+            className="bg-blue-600 text-white px-3 py-2"
+            onClick={() => setAddPhotoMode(!addPhotoMode)}
+          >
+            {addPhotoMode ? "Cancel" : "Add Photo"}
+          </button>
+        )}
+      </div>
+      <div className="">
+        {addPhotoMode ? (
+          <p>Photo widget</p>
+        ) : (
+          <div className="grid gap-3 grid-cols-5">
+            {profile.photos?.map((photo) => (
+              <div key={photo?.id}>
+                <img src={photo?.url} alt="Photo" />
+              </div>
+            ))}
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
