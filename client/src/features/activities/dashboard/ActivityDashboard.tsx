01c1c2b80555f5b27626715d706df7ebbb7bfc7b
mldamico <matias.lioneldamico@gmail.com>
2023-09-06 09:48:50 -0300

    Placeholder activity List

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,10 +2,10 @@ import { observer } from "mobx-react-lite";
 import { useStore } from "../../../app/stores/store";
 import { ActivityList } from "./ActivityList";
 import { useEffect, useState } from "react";
-import { Loading } from "../../../app/layout/Loading";
 import { ActivityFilters } from "./ActivityFilters";
 import { PagingParams } from "../../../types/pagination";
 import InfiniteScroll from "react-infinite-scroller";
+import { ActivityListItemPlaceholder } from "./ActivityListItemPlaceholder";
 
 export const ActivityDashboard = observer(() => {
   const { activityStore } = useStore();
@@ -23,25 +23,32 @@ export const ActivityDashboard = observer(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [loadActivities, activityRegistry.size]);
 
-  if (activityStore.loadingInitial && !loadingNext)
-    return <Loading content="Loading activities..." />;
-
   return (
     <div className="grid gap-4 grid-cols-[1.2fr_0.6fr]">
-      <div className="">
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={handleGetNext}
-          hasMore={
-            !loadingNext &&
-            !!pagination &&
-            pagination.currentPage < pagination.totalPages
-          }
-          initialLoad={false}
-        >
-          <ActivityList />
-        </InfiniteScroll>
-      </div>
+      {activityStore.loadingInitial &&
+      activityRegistry.size === 0 &&
+      !loadingNext ? (
+        <>
+          <ActivityListItemPlaceholder />
+          <ActivityListItemPlaceholder />
+        </>
+      ) : (
+        <div className="">
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={handleGetNext}
+            hasMore={
+              !loadingNext &&
+              !!pagination &&
+              pagination.currentPage < pagination.totalPages
+            }
+            initialLoad={false}
+          >
+            <ActivityList />
+          </InfiniteScroll>
+        </div>
+      )}
+
       <div className="">
         <ActivityFilters />
       </div>
